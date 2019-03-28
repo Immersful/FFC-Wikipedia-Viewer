26b7cc69f48ce126cc6d18e86883011c38b17de2
Christopher Simpson <ImmersfulFlame@gmail.com>
2019-03-28 11:15:19 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('#search').click(function(){
+    var searchTerm= $('#searchTerm').val();
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+    
+    $.ajax({
+      type:"GET",
+      url: url,
+      async: false,
+      dataType: "Json",
+      success: function(data){
+       //console.log(data);
+        $('#output').html('')
+        for(var i=0;i<data[1].length;i++)
+        $('#output').prepend("<li><a href= "+data[3][i]+">"+data[1][i]+ "</a><p>"+data[2][i]+"</p></li>");
+      },
+      error: function(errorMessage){
+        alert("Error");
+      }
+    });
+  });
+});
